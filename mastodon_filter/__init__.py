cfdd386cd67930d9d9fe67beea1f9604bb5ddeaf
Harshad Sharma <harshad@sharma.io>
2023-05-06 23:56:32 +0530

    Ignore empty lines

--- mastodon_filter/__init__.py
+++ mastodon_filter/__init__.py
@@ -77,6 +77,8 @@ class MastodonFilters:
         """
         params = {}
         for i, keyword in enumerate(keywords):
+            if not keyword:
+                continue
             params[f"keywords_attributes[{i}][keyword]"] = keyword
             params[f"keywords_attributes[{i}][whole_word]"] = True
         return params

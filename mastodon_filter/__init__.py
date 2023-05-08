e8b749fdc6fbd6b70e13226028df315ac6749e17
Harshad Sharma <harshad@sharma.io>
2023-05-08 17:40:43 +0530

    Modifications for py2app packaging

--- mastodon_filter/__init__.py
+++ mastodon_filter/__init__.py
@@ -1,3 +1,6 @@
 """
 Manage keyword filters on Mastodon from command-line.
 """
+# Eager imports to satisfy py2app
+import charset_normalizer as _cn
+import charset_normalizer.md__mypyc as _cn_md__mypyc

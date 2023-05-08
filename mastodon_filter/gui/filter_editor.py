858747f95d58b1f734e045609d066ee18a8dc502
Harshad Sharma <harshad@sharma.io>
2023-05-08 12:51:21 +0530

    Use thread for network task

--- mastodon_filter/gui/filter_editor.py
+++ mastodon_filter/gui/filter_editor.py
@@ -3,8 +3,15 @@ Mastodon FilterEditor.
 """
 # pylint: disable=attribute-defined-outside-init
 import json
+import threading
 import tkinter as tk
+from tkinter import messagebox
+
 import customtkinter as ctk
+
+from mastodon_filter.api import MastodonFilters
+from mastodon_filter.config import get_config
+from mastodon_filter.errors import extract_error_message
 
 
 class FilterEditor(ctk.CTkFrame):
@@ -14,6 +21,7 @@ class FilterEditor(ctk.CTkFrame):
         """Initialize Frame."""
         ctk.CTkFrame.__init__(self, parent, **kwargs)
         self.parent = parent
+        self.cached_filters_path = tk.StringVar()
         self.init_ui()
 
     def init_ui(self):
@@ -24,10 +32,14 @@ class FilterEditor(ctk.CTkFrame):
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
         self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure(4, weight=1)
 
         self.label = ctk.CTkLabel(self, text="Keywords")
-        self.label.grid(row=0, column=0, columnspan=4, sticky="nsew")
+        self.label.grid(row=0, column=0, columnspan=5, sticky="nsew")
+
         self.init_editor()
+        self.init_buttons()
+
         self.grid(row=0, column=1, columnspan=3, sticky="nsew")
 
     def init_editor(self):
@@ -38,10 +50,29 @@ class FilterEditor(ctk.CTkFrame):
             autoseparators=True,
             maxundo=-1,
         )
-        self.editor.grid(row=1, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+        self.editor.grid(row=1, column=0, columnspan=5, sticky="nsew", padx=5, pady=5)
+
+    def init_buttons(self):
+        """Initialize buttons."""
+
+        # self.button_cancel = ctk.CTkButton(
+        #     self,
+        #     text="Cancel",
+        #     fg_color="#888888",
+        #     # command=self.cancel,
+        # )
+        # self.button_cancel.grid(row=2, column=0, sticky="nsew", padx=5, pady=5)
+
+        self.button_save = ctk.CTkButton(
+            self,
+            text="Save",
+            command=self.save_filter,
+        )
+        self.button_save.grid(row=2, column=4, sticky="nsew", padx=5, pady=5)
 
     def load_filter(self, cached_filters_path, title):
         """Load filter."""
+        self.cached_filters_path = cached_filters_path
         with open(cached_filters_path, "r", encoding="utf-8") as file:
             filters = json.load(file)
         for filter in filters:  # pylint: disable=redefined-builtin
@@ -55,3 +86,36 @@ class FilterEditor(ctk.CTkFrame):
         self.editor.delete("1.0", tk.END)
         self.editor.insert(tk.END, keywords)
         self.editor.edit_reset()
+
+    def save_filter(self):
+        """Save filter."""
+        self.button_save.configure(text="Saving...", state="disabled")
+        self.editor.configure(state="disabled")
+        thread = threading.Thread(target=self.save_filter_thread)
+        thread.daemon = True
+        thread.start()
+
+    def save_filter_thread(self):
+        """Save filter in background."""
+        title = self.parent.filter_list.current_filter.get()
+        keywords = self.editor.get("1.0", tk.END)
+        keywords_list = keywords.split("\n")
+        keywords_list = [kw for kw in keywords_list if kw]
+
+        config = get_config()
+        if not config.api_base_url or not config.access_token:
+            return
+
+        print(f"Saving filter {title} with {len(keywords_list)} keywords.")
+        try:
+            filters = MastodonFilters(config)
+            filters.sync(title=title, keywords=keywords_list)
+            self.parent.filter_list.load_filters()
+            self.load_filter(self.cached_filters_path, title)
+        except Exception as err:  # pylint: disable=broad-except
+            error_message = extract_error_message(err)
+            messagebox.showerror("Error", error_message)
+        finally:
+            self.button_save.configure(text="Save", state="normal")
+            self.editor.configure(state="normal")
+            print("Done.")

fd307eb7ac8b674ada0f2f674de26c34f8f59f34
Harshad Sharma <harshad@sharma.io>
2023-05-08 23:37:17 +0530

    Ignore charset_normalizer import if not found.

--- mastodon_filter/__init__.py
+++ mastodon_filter/__init__.py
@@ -1,6 +1,9 @@
 """
 Manage keyword filters on Mastodon from command-line.
 """
-# Eager imports to satisfy py2app
-import charset_normalizer as _cn
-import charset_normalizer.md__mypyc as _cn_md__mypyc
+try:
+    # Eager imports to satisfy py2app
+    import charset_normalizer as _cn
+    import charset_normalizer.md__mypyc as _cn_md__mypyc
+except ImportError:
+    pass

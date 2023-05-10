0887deafd026de30f093da77e7c3d3ce6cfebe41
Harshad Sharma <harshad@sharma.io>
2023-05-10 10:58:31 +0530

    Add commands to File menu

--- mastodon_filter/newgui/app.py
+++ mastodon_filter/newgui/app.py
@@ -23,14 +23,49 @@ class MastodonFilterGUI(ctk.CTk):
         self.menu = tk.Menu(self)
         self.config(menu=self.menu)
 
+        # File menu
         self.file_menu = tk.Menu(self.menu, tearoff=0)
+        self.file_menu.add_command(label="Import Filter", command=self.import_filter)
+        self.file_menu.add_command(label="Export Filter", command=self.export_filter)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(
+            label="Backup Filters",
+            command=self.backup_filters,
+        )
+        self.file_menu.add_command(
+            label="Restore Filters", command=self.restore_filters
+        )
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Preferences", command=self.open_preferences)
+        self.file_menu.add_separator()
         self.file_menu.add_command(label="Exit", command=self.quit)
         self.menu.add_cascade(label="File", menu=self.file_menu)
 
+        # Help menu
         self.help_menu = tk.Menu(self.menu, tearoff=0)
         self.help_menu.add_command(label="About", command=self.open_about_page)
         self.help_menu.add_command(label="Issue Tracker", command=self.open_issues_page)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
+
+    def import_filter(self):
+        """Import Filter."""
+        raise NotImplementedError("Import Filter not implemented yet.")
+
+    def export_filter(self):
+        """Export Filter."""
+        raise NotImplementedError("Export Filter not implemented yet.")
+
+    def backup_filters(self):
+        """Backup Filters."""
+        raise NotImplementedError("Backup Filters not implemented yet.")
+
+    def restore_filters(self):
+        """Restore Filters."""
+        raise NotImplementedError("Restore Filters not implemented yet.")
+
+    def open_preferences(self):
+        """Open Preferences."""
+        raise NotImplementedError("Preferences not implemented yet.")
 
     def open_about_page(self):
         """Open About page."""

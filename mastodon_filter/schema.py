1a875f7d3279ad546d3de31b846c5993aea492a9
Harshad Sharma <harshad@sharma.io>
2023-05-10 17:48:14 +0530

    Add schemas for context, status and filter

--- mastodon_filter/schema.py
+++ mastodon_filter/schema.py
@@ -4,10 +4,14 @@ from typing import Optional
 
 @dataclass
 class Keyword:
+    """
+    Keyword.
+    """
+
     keyword: str
     whole_word: bool = True
     delete: bool = False
-    id: Optional[str] = None
+    id: Optional[str] = None  # pylint: disable=invalid-name
 
     def __post_init__(self):
         self.keyword = self.keyword.strip()
@@ -17,3 +21,51 @@ class Keyword:
 
     def __eq__(self, o):
         return self.keyword == o.keyword
+
+
+@dataclass
+class Context:
+    """
+    Context.
+    """
+
+    home: bool = False
+    notifications: bool = False
+    public: bool = False
+    thread: bool = False
+    account: bool = False
+
+
+@dataclass
+class Status:
+    """
+    Status.
+    """
+
+    id: str  # pylint: disable=invalid-name
+    status_id: str
+
+
+@dataclass
+class Filter:
+    """
+    Filter.
+    """
+
+    title: str
+    context: Context
+    expires_at: Optional[int] = None
+    filter_action: str = "hide"
+    keywords: list[Keyword]
+    statuses: list[Status]
+    id: Optional[str] = None  # pylint: disable=invalid-name
+
+    def __post_init__(self):
+        self.title = self.title.strip()
+        self.keywords = [Keyword(**asdict(kw)) for kw in self.keywords]
+
+    def __str__(self):
+        return self.title
+
+    def __eq__(self, o):
+        return self.title == o.title
